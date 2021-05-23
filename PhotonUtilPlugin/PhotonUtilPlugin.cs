13d610a796a59c91e56e4f073efa9c2e42b3d159
brajm008 <ujkkju117@outlook.com>
2021-05-24 03:34:22 +0930

    bump version.

--- PhotonUtilPlugin/PhotonUtilPlugin.cs
+++ PhotonUtilPlugin/PhotonUtilPlugin.cs
@@ -12,7 +12,7 @@ namespace PhotonUtil
         /// Identifier used to include this plugin as a bepinex dependency.
         /// </summary>
         public const string Guid = "org.hollofox.plugins.PhotonUtil";
-        private const string Version = "1.0.2.0";
+        private const string Version = "1.0.3.0";
 
         private static readonly Guid AuthorId = System.Guid.NewGuid();
         
